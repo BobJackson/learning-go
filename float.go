666dd30b0d9d9130bfa38a32829ca8e94af93242
Bob Jackson <wangyousong@9191offer.com>
2024-02-18 22:01:11 +0800

    to learn  floating-point

--- float.go
+++ float.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
 	third := 1.0 / 3.0
@@ -9,4 +12,12 @@ func main() {
 	piggyBank := 0.1
 	piggyBank += 0.2
 	fmt.Println(piggyBank)
+
+	fmt.Println(math.Abs(piggyBank-0.3) < 0.0001)
+
+	piggyBankCopy := 0.0
+	for i := 0; i < 11; i++ {
+		piggyBankCopy += 0.1
+	}
+	fmt.Println(piggyBankCopy)
 }
